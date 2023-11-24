6ae52bf5b09de1319c07b1b9d36e0d57c1d84c6a
Murilo Pereira <62906152+murilopereira006@users.noreply.github.com>
2023-11-23 21:39:02 -0300

    função soma de números multiplos de 3 ou 5

---
+++ Multiplos.js
@@ -0,0 +1,13 @@
+const numeroLimite = 1000;
+
+const multiplos = (numeroLimite) => {
+    let arrayDeMultiplosDe3ou5 = [];
+
+    for (let i = 0; i < numeroLimite; ++i) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            arrayDeMultiplosDe3ou5.push(i);
+        }
+    }
+
+    return arrayDeMultiplosDe3ou5.reduce((accumulator, value) => accumulator + value, 0);
+};
